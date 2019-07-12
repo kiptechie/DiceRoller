779ae1f7d169a4123e8b7e28ec65219b8722f28f
Timothy Serem <kipkorirserem3@gmail.com>
2019-07-12 21:05:24 +0300

    added firebase cloud functionality

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,6 +8,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
                 rollDice()
             }
+        diceImage = findViewById(R.id.dice_image)
+
     }
 
     private fun rollDice() {
@@ -31,7 +35,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-
         diceImage.setImageResource(drawableResource)
     }
 }

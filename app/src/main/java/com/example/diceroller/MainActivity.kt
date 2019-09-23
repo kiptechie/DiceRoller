72b7e5005fe3906637c61d996450767e12eafad3
Timothy K. Serem <kipkorirtimserem@outlook.com>
2019-09-23 10:56:04 +0300

    crash fix

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,25 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-// import com.google.android.gms.ads.AdRequest
-// import com.google.android.gms.ads.AdView
-// import com.google.android.gms.ads.MobileAds
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    // lateinit var mAdView : AdView
     lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-       // MobileAds.initialize(this) {}
-
-       // mAdView = findViewById(R.id.adView)
-       // val adRequest = AdRequest.Builder().build()
-       // mAdView.loadAd(adRequest)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
